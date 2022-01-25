0ec54eb98440c5c66a49dca66243242fffa539a3
Ismat <ismatmahmood313@gmail.com>
2022-01-25 22:39:15 +0000

    completed 3 tests currently working on 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    let repeats = "";
 
+    if (num === 0) {
+        return repeats;
+    }
+    else if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        while (num != 0) {
+            repeats = repeats + string;
+            num--;
+        }
+        return repeats;
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, remove) {
+    for (i=0; i< arr.length;i++){
+        if (remove.includes(arr[i]))
+            console.log("true")
+        else
+            console.log("false")
+    }
+   // const result = arr.filter(x => remove.includes(x) != true )
+    
+   // return result;
 };
 
 // Do not edit below this line
