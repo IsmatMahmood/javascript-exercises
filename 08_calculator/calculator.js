424d816696578d2f38664f64b8d257865d163863
Ismat <ismatmahmood313@gmail.com>
2022-01-26 13:42:12 +0000

    completed up to 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	let sum = 0;
+  for (i = 0; i< numArr.length; i++){
+    sum += numArr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let sum = numArr[0];
+  for (i = 1; i< numArr.length; i++){
+    sum *= numArr[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return Math.pow(num, power);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0 || num == 1)
+    return 1;
+  else
+  {
+    let fact = num * (num- 1);
+    num--;
+    while (num > 1){
+      fact = fact * (num - 1);
+      num--;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
